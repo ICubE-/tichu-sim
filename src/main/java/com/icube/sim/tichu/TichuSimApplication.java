73dfc909e24d84218aa992c4f2edf8a4bc4e9ebf
ICubE- <jehoyeon01@gmail.com>
2025-12-27 23:29:47 +0900

    Initial commit

---
+++ src/main/java/com/icube/sim/tichu/TichuSimApplication.java
@@ -0,0 +1,11 @@
+package com.icube.sim.tichu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TichuSimApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TichuSimApplication.class, args);
+    }
+}
---
+++ src/test/java/com/icube/sim/tichu/TichuSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icube.sim.tichu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TichuSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
