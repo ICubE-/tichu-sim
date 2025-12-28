51f8d037a0dd3d30b2fd2d8c40b68c8f46a66b78
ICubE- <jehoyeon01@gmail.com>
2025-12-28 22:59:08 +0900

    Add user service - Register user

---
+++ src/main/java/com/icube/sim/tichu/users/User.java
@@ -0,0 +1,34 @@
+package com.icube.sim.tichu.users;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", schema = "tichu")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
---
+++ src/main/java/com/icube/sim/tichu/users/UserMapper.java
@@ -0,0 +1,9 @@
+package com.icube.sim.tichu.users;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDto toDto(User user);
+    User toEntity(RegisterUserRequest request);
+}
