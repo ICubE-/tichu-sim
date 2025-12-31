372edd130d458e816ee51de0dec62b2b9406d6b1
ICubE- <jehoyeon01@gmail.com>
2025-12-31 20:08:30 +0900

    Add logging filter

---
+++ src/main/java/com/icube/sim/tichu/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.icube.sim.tichu;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, @NonNull HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        System.out.println(request.getMethod() + " " + request.getRequestURI());
+        filterChain.doFilter(request, response);
+        System.out.println(response.getStatus());
+    }
+}
