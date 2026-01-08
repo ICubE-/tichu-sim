4ba48957c415c201e6a1016a7f2d43d3af01ad42
ICubE- <jehoyeon01@gmail.com>
2026-01-08 19:21:19 +0900

    Use Axios interceptor

---
+++ frontend/src/useAxios.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import {useEffect} from 'react';
+import {useAuth} from './useAuth.jsx';
+
+const api = axios.create({
+  baseURL: '/api',
+  withCredentials: true,
+});
+
+export const useAxios = () => {
+  const {accessToken, setAccessToken, logout} = useAuth();
+
+  useEffect(() => {
+    const requestIntercept = api.interceptors.request.use(
+      (config) => {
+        if (!config.headers['Authorization'] && accessToken !== null) {
+          config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    const responseIntercept = api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (prevRequest?.sent && error?.response?.status !== 401) {
+          return Promise.reject(error);
+        }
+
+        prevRequest.sent = true;
+        try {
+          const res = await axios.post('/api/auth/refresh', {}, {withCredentials: true});
+          const newAccessToken = res.data.accessToken;
+
+          setAccessToken(newAccessToken);
+
+          prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return api(prevRequest);
+        } catch (refreshError) {
+          logout();
+          return Promise.reject(refreshError);
+        }
+      }
+    );
+
+    return () => {
+      api.interceptors.request.eject(requestIntercept);
+      api.interceptors.response.eject(responseIntercept);
+    };
+  }, [accessToken, setAccessToken, logout]);
+
+  return api;
+};
