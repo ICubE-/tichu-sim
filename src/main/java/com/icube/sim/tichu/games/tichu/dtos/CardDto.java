7f298872ef162e4739f6cd550212f42f7a8e10d4
ICubE- <jehoyeon01@gmail.com>
2026-02-12 21:51:57 +0900

    Add `Nullable` to DTOs

--- src/main/java/com/icube/sim/tichu/games/tichu/dtos/CardDto.java
+++ src/main/java/com/icube/sim/tichu/games/tichu/dtos/CardDto.java
@@ -2,18 +2,21 @@ package com.icube.sim.tichu.games.tichu.dtos;
 
 import com.icube.sim.tichu.games.tichu.cards.CardSuit;
 import lombok.Data;
+import org.jspecify.annotations.Nullable;
 
 @Data
 public class CardDto {
     private CardType type;
+    @Nullable
     private CardSuit suit;
+    @Nullable
     private Integer rank;
 
     public CardDto(CardType type) {
         this(type, null, null);
     }
 
-    public CardDto(CardType type, CardSuit suit, Integer rank) {
+    public CardDto(CardType type, @Nullable CardSuit suit, @Nullable Integer rank) {
         this.type = type;
         this.suit = suit;
         this.rank = rank;
