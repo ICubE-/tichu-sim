81a071db747dd7edf6d220fbfe638e03bd9c46e4
ICubE- <jehoyeon01@gmail.com>
2026-02-25 20:23:30 +0900

    Detach common exception handlers

---
+++ src/main/java/com/icube/sim/tichu/games/common/exceptions/GameExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.icube.sim.tichu.games.common.exceptions;
+
+import com.icube.sim.tichu.common.websocket.ErrorMessage;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice(basePackages = "com.icube.sim.tichu.games")
+public class GameExceptionHandler {
+    @MessageExceptionHandler(GameHasAlreadyStartedException.class)
+    @SendToUser("/queue/errors")
+    public ErrorMessage handleGamePlaying() {
+        return new ErrorMessage("Game has already been playing.");
+    }
+
+    @MessageExceptionHandler(ImmutableGameRuleException.class)
+    @SendToUser("/queue/errors")
+    public ErrorMessage handleImmutableGameRule() {
+        return new ErrorMessage("Game rule is now immutable.");
+    }
+
+    @MessageExceptionHandler(InvalidGameRuleException.class)
+    @SendToUser("/queue/errors")
+    public ErrorMessage handleInvalidGameRule() {
+        return new ErrorMessage("Game rule is invalid.");
+    }
+
+    @MessageExceptionHandler(GameNotFoundException.class)
+    @SendToUser("/queue/errors")
+    public ErrorMessage handleGameNotFound() {
+        return new ErrorMessage("Game not found.");
+    }
+
+    @MessageExceptionHandler(InvalidTimeOfActionException.class)
+    @SendToUser("/queue/errors")
+    public ErrorMessage handleInvalidTimeOfAction() {
+        return new ErrorMessage("The action cannot be performed at this time.");
+    }
+}
--- src/main/java/com/icube/sim/tichu/games/tichu/controllers/TichuSmallTichuController.java
+++ src/main/java/com/icube/sim/tichu/games/tichu/controllers/TichuSmallTichuController.java
@@ -2,11 +2,11 @@ package com.icube.sim.tichu.games.tichu.controllers;
 
 import com.icube.sim.tichu.common.websocket.ErrorMessage;
 import com.icube.sim.tichu.games.tichu.TichuService;
-import com.icube.sim.tichu.games.common.exceptions.GameNotFoundException;
 import com.icube.sim.tichu.games.tichu.exceptions.InvalidTichuDeclarationException;
-import com.icube.sim.tichu.games.common.exceptions.InvalidTimeOfActionException;
 import lombok.AllArgsConstructor;
-import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
@@ -23,18 +23,6 @@ public class TichuSmallTichuController {
             Principal principal
     ) {
         gameService.smallTichu(roomId, principal);
-    }
-
-    @MessageExceptionHandler(GameNotFoundException.class)
-    @SendToUser("/queue/errors")
-    public ErrorMessage handleGameNotFound() {
-        return new ErrorMessage("Game not found.");
-    }
-
-    @MessageExceptionHandler(InvalidTimeOfActionException.class)
-    @SendToUser("/queue/errors")
-    public ErrorMessage handleInvalidTimeOfAction() {
-        return new ErrorMessage("The action cannot be performed at this time.");
     }
 
     @MessageExceptionHandler(InvalidTichuDeclarationException.class)
